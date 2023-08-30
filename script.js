5d5353b35224054dd61bddc09d5ec064a7e99934
Bhumi <bhumione9545@gmail.com>
2023-08-30 13:53:49 +0530

    Adding final chanegs

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const pencilCont = document.querySelector(".pencil-container");
 const eraserCont = document.querySelector(".eraser-container");
 const textlCont = document.querySelector(".text-container");
 const stickylCont = document.querySelector(".sticky-notes-container");
-const undoEle = document.querySelector(".pencil-container img");
-const redoEle = document.querySelector(".pencil-container img");
-const downloadEle = document.querySelector(".pencil-container img");
-const uploadEle = document.querySelector(".pencil-container img");
+const undoCont = document.querySelector(".undo-container");
+const redoCont = document.querySelector(".redo-container");
+const downloadCont = document.querySelector(".download-container");
+const uploadCont = document.querySelector(".upload-container");
 const pencilOptionsDiv = document.querySelector(".pencil-options-div")
 const earaserOptionsDiv = document.querySelector(".eraser-options-div")
 const strokeWidthInput = document.querySelector(".pencil-width input");
@@ -14,6 +14,7 @@ const easerWidthInput = document.querySelector(".eraser-width input");
 const pencilColorArr = document.querySelectorAll(".pencil-color-item");
 const rangeInputEle = document.getElementById("vol");
 const easerRangeInputEle = document.getElementById("easervol");
+const fileInput = document.getElementById("fileInput");
 const body = document.querySelector("body");
 const canvas = document.querySelector("canvas");
 
@@ -21,7 +22,8 @@ const canvas = document.querySelector("canvas");
 let currentColor = "white"
 let currentPenWidth = 5;
 let currentEeaserWidth = 20;
-
+let undoRedoCache = [canvas.toDataURL()];
+let currentUrlIndex = 0;
 
 
 isPencilActive = false;
@@ -60,7 +62,7 @@ function handleEaraserCont() {
         earaserOptionsDiv.style.display = "block";
         isPainting = false;
         changeCursorStyle("eraser", false);
-        ctx.strokeStyle = "#0b0b0b";
+        ctx.strokeStyle = "#000000";
         eraserCont.classList.add("active-tool-container");
         isEraserActive = true;
     } else {
@@ -106,6 +108,65 @@ easerRangeInputEle.addEventListener("change", (e) => {
 })
 
 
+downloadCont.addEventListener("click", () => {
+    let url = canvas.toDataURL("image/jpeg"); // Specify the MIME type here
+    const blob = dataURItoBlob(url);
+    const link = document.createElement("a");
+    // Convert blob to downloadable URL
+    link.href = URL.createObjectURL(blob);
+    link.download = "whiteboard.jpeg";
+    // Trigger the download
+    link.click();
+});
+
+function dataURItoBlob(dataURI) {
+    // Split the data URI into the MIME type and data
+    const splitDataURI = dataURI.split(",");
+    // Extract the MIME type (e.g., "image/jpeg")
+    const mime = splitDataURI[0].match(/:(.*?);/)[1];
+    // Convert the base64-encoded data to a Uint8Array
+    const byteString = atob(splitDataURI[1]);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+        uint8Array[i] = byteString.charCodeAt(i);
+    }
+    // Create a Blob from the Uint8Array
+    return new Blob([uint8Array], { type: mime });
+}
+
+
+uploadCont.addEventListener("click", () => {
+    fileInput.click();
+})
+
+
+
+fileInput.addEventListener("change", (event) => {
+    console.log("inside upload")
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = (loadEvent) => {
+            const image = new Image();
+            image.src = loadEvent.target.result;
+
+            // When the image is loaded, draw it on the canvas
+            image.onload = () => {
+                canvas.width = image.width;
+                canvas.height = image.height;
+                ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+            };
+        };
+
+        // Read the uploaded file as a data URL
+        reader.readAsDataURL(file);
+    }
+});
+
+
+
 // drawing js -----------------------------------------------
 
 // Getting the offset values of canvas
@@ -134,13 +195,14 @@ const draw = (e) => {
     if (isPainting && isPencilActive || isEraserActive) {
         ctx.lineWidth = lineWidth;
         if (isEraserActive) {
-            ctx.strokeStyle = "#0b0b0b";
+            ctx.strokeStyle = "#000000";
             ctx.lineWidth = currentEeaserWidth;
         } else {
             ctx.lineWidth = currentPenWidth;
             ctx.strokeStyle = `${currentColor}`;
         }
-        ctx.lineCap = 'round';
+        ctx.lineCap = 'round'
+        ctx.lineJoin = 'round'
         ctx.lineTo(e.clientX - canvasOffsetX - 0, e.clientY - -30);
         ctx.stroke();
     }
@@ -155,14 +217,44 @@ canvas.addEventListener('mousedown', (e) => {
     clearCanvas()
 });
 
+canvas.addEventListener('mousemove', draw);
+
 canvas.addEventListener('mouseup', e => {
     isPainting = false;
     ctx.stroke();
     ctx.beginPath();
+    let url = canvas.toDataURL();
+    undoRedoCache.push(url);
+    currentUrlIndex = undoRedoCache.length - 1;
 });
 
-canvas.addEventListener('mousemove', draw);
 
+undoCont.addEventListener("click", () => {
+    if (currentUrlIndex > 0) {
+        currentUrlIndex--;
+        renderUrlOnCanvas(currentUrlIndex);
+    }
+
+})
+
+redoCont.addEventListener("click", () => {
+    if (currentUrlIndex < undoRedoCache.length - 1) {
+        currentUrlIndex++;
+        renderUrlOnCanvas(currentUrlIndex);
+    }
+
+})
+
+
+function renderUrlOnCanvas(index) {
+    let url = undoRedoCache[index];
+    let img = new Image();
+    img.src = url;
+    img.onload = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+}
 
 
 function clearCanvas() {
